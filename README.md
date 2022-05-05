336131c194435eee066c670e7689763a41be291a
JoannafKo <koj097@newschool.edu>
2022-05-05 12:05:20 -0400

    update

--- README.md
+++ README.md
@@ -3,8 +3,15 @@ This is a quiz to help both novices and Latin students learn about the Latin lan
  ## About
 More in depth details about the project.
 ## Process & Documentation
-I first got creating a repo over with, because I would forget how to do so later on in the project
+I first got creating a repo over with, because I would forget how to do so later on in the project. Then I looked into a video series about making a quiz or game for HTML, CSS, and Javascript, and I followed it while making my own customization
+
+I sprinkled in Latin phrases and words throughout the website, to fit with the language theme. I even tweeked some of them a bit. I took the phrase "ex luna scientia," which means "knowledge from the moon," and turned it into "ex lingua scientia," which means "knowledge from language"
+
+I created a page reserved for the questions. I rant into a bit a trouble with creating the functions, mainly because I mispelled it.
+
 ## Challenges & Struggles
-What challenges and struggles did you face?
+First challenge I got involved in was with the folders. I initially tried to catergorize the extra HTML and CSS files into different folders, but when I tried to attatch the style sheet to them, the script wouldn't visually show up. I ultimately gave up on the folders, it made things too complicated.
 ## References
-Credit resources appropriately
+https://youtube.com/playlist?list=PLDlWc9AfQBfZIkdVaOQXi1tizJeNJipEx
+
+https://fonts.google.com/specimen/Cinzel#standard-styles
---
+++ main.js
@@ -0,0 +1,100 @@
+// test
+console.log("Answer");
+
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "asdasdsadsda",
+        choice1: "<asdadsasd>",
+        choice2: '<asdadsasd>',
+        choice3: '<asdadsasd>',
+        choice4: '<asdadsasd>',
+        answer: 1
+    },
+    {
+        question: "435345435",
+        choice1: '<345345435>',
+        choice2: '<345345435>',
+        choice3: '<345345435>',
+        choice4: '<345345435>',
+        answer: 1
+    },
+    {
+        question: "DSHJKDSJDKSHJ",
+        choice1: '<SDHJDS>',
+        choice2: '<SDHJDS>',
+        choice3: '<SDHJDS>',
+        choice4: '<SDHJDS>',
+        answer: 1
+    }
+]
+
+// constants
+
+const points = 5;
+const numberQuestions = 3;
+
+// startGame();
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length === 0 || questionCounter >= numberQuestions) {
+        //go to the end page
+        return window.location.assign('end.html');
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+
+choices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        // console.log(classToApply);
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+          }, 1500);
+    });
+});
+
+
+startGame();
